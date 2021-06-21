7f371f9fd552d9e772fc513db82a954105fe02a2
Mallikarjun Hanagandi <mallikarjun.h1410@gmail.com>
2021-06-21 12:59:32 +0530

    War Card Game UI Construction
    Base Project Is Added for War Card Game UI Construction

---
+++ War Card Game UI Construction/War Card Game UI Construction/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  War Card Game UI Construction
+//
+//  Created by EOO61 on 21/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ War Card Game UI Construction/War Card Game UI Construction/War_Card_Game_UI_ConstructionApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_Game_UI_ConstructionApp.swift
+//  War Card Game UI Construction
+//
+//  Created by EOO61 on 21/06/21.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_Game_UI_ConstructionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
