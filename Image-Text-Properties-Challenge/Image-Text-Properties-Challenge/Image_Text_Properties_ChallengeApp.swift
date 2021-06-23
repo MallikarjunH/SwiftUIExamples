3858bcb1793dca8eda1405cf1f9666d213f7ce00
Mallikarjun Hanagandi <mallikarjun.h1410@gmail.com>
2021-06-23 09:29:08 +0530

    Image+Text+Stack with Modifiers

---
+++ Image-Text-Properties-Challenge/Image-Text-Properties-Challenge/Image_Text_Properties_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_Text_Properties_ChallengeApp.swift
+//  Image-Text-Properties-Challenge
+//
+//  Created by EOO61 on 22/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Image_Text_Properties_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
