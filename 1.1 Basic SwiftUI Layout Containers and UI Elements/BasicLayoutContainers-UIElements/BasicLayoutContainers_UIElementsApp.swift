5e093b908a8f21b918906088182237f3e0c037c9
Mallikarjun Hanagandi <mallikarjun.h1410@gmail.com>
2021-07-02 11:52:26 +0530

    Basic Layouts and Challenge Example
    Basic Layouts and Challenge Example Code and Samples

---
+++ 1.1 Basic SwiftUI Layout Containers and UI Elements/BasicLayoutContainers-UIElements/BasicLayoutContainers_UIElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicLayoutContainers_UIElementsApp.swift
+//  BasicLayoutContainers-UIElements
+//
+//  Created by EOO61 on 02/07/21.
+//
+
+import SwiftUI
+
+@main
+struct BasicLayoutContainers_UIElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
