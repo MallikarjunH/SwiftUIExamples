bdd702ad078f38972ffa465bc381efe416dd625e
Mallikarjun Hanagandi <mallikarjun.h1410@gmail.com>
2022-06-08 12:46:00 +0530

    rearranged folder items

---
+++ 1. CodeWithChris Tutorials/2. Views and Containers/Views and Containers/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  Views and Containers
+//
+//  Created by EOO61 on 18/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+      //  Image("natureImg").resizable()
+        
+       // Image("natureImg").resizable().aspectRatio(contentMode: .fill)
+      //  Image("natureImg").resizable().aspectRatio(contentMode: .fit)
+        /*
+        VStack{
+            Image("natureImg").resizable().aspectRatio(contentMode: .fit)
+            Text("Nature Image")
+        } */
+        
+        /*
+        HStack{
+            Image("natureImg").resizable().aspectRatio(contentMode: .fit)
+            Text("Nature Image")
+        } */
+        /*
+        ZStack{
+            Image("natureImg").resizable().aspectRatio(contentMode: .fit)
+            Text("Nature Image")
+        }
+        */
+        
+        VStack{
+            Spacer()
+            Image("natureImg").resizable().aspectRatio(contentMode: .fit)
+            Spacer()
+            VStack{
+                Text("Nature Image").foregroundColor(.green)
+                Text("Simple Example").bold()
+            }
+            Spacer()
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TableView Examples/TableView-Example/TableView-Example/TableView_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableView_ExampleApp.swift
+//  TableView-Example
+//
+//  Created by EOO61 on 07/06/22.
+//
+
+import SwiftUI
+
+@main
+struct TableView_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
