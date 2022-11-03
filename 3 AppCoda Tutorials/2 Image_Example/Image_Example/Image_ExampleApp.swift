26ae9bbef0e9f99d8a7ab0189855430789a6f3e1
Mallikarjun Hanagandi <mallikarjun.h1410@gmail.com>
2022-11-03 10:06:10 +0530

    updates folder structure and added some projects

---
+++ 3 AppCoda Tutorials/2 Image_Example/Image_Example/Image_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_ExampleApp.swift
+//  Image_Example
+//
+//  Created by EOO61 on 10/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Image_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 4 CodeCat Tutorials/VStack and HStack login view/VStack and HStack login view/VStack_and_HStack_login_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  VStack_and_HStack_login_viewApp.swift
+//  VStack and HStack login view
+//
+//  Created by EOO61 on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct VStack_and_HStack_login_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
