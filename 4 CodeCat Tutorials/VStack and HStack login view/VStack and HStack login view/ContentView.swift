3c0cc6e983bf14fbc0039c6f9ae4c9edc5aaf6c7
Mallikarjun Hanagandi <mallikarjun.h1410@gmail.com>
2022-11-07 12:39:13 +0530

    Login UI added using VStack & HStack

--- 4 CodeCat Tutorials/VStack and HStack login view/VStack and HStack login view/ContentView.swift
+++ 4 CodeCat Tutorials/VStack and HStack login view/VStack and HStack login view/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var userName:String = String()
+    @State private var password:String = String()
+    
+    //Notes: SwiftUI manages the storage of any property you as a state. When the state value changes, the view invalidates its appearance and recomputes the body. use the state as the singgle source of truth for a given view.
+    //A State instance isnt the value itself. It means of reading and writing the value. To access a states underlying value, use its variable name, which returns the wrappedValue property value
+    //You should only acess a state property from inside the view's body or from methods called by it. For this reason,  declare your state properties as private to prevent clients of your view from accessing it. It is safe to mutate state properties from any thread.
+    
     var body: some View {
+        /*
+        Text("LOGIN")
+        Text("Explore SWIFT UI")
+        //Note: If you write this 2 lines It will take by default as HStack. These 2 words will display on same line
+        */
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            Text("LOGIN").font(.largeTitle).padding()
+            Text("Explore SWIFT UI").font(.subheadline).padding()
+            
+            TextField("UserName", text: $userName)
+                .padding()
+                .background(.gray)
+                .cornerRadius(4.0)
+            
+            SecureField("Password", text: $password)
+                .padding()
+                .background(.gray)
+                .cornerRadius(4.0)
+            
+            HStack {
+                //Button(_ title:action:)
+                Button("Validate") {
+                    debugPrint("validate Crdentials")
+                }
+                Spacer()
+                //OR another way to create button and action i.e Button(action:label:)
+                Button {
+                    debugPrint("clicked on forgot password")
+                } label: {
+                    Text("Forgot Password")
+                }
+                
+                
+            }
+            
+        }.padding()
+        
     }
 }
 
