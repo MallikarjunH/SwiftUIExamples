2b6a7760f7234732cf640432f3fa89518161c8be
Mallikarjun Hanagandi <mallikarjun.h1410@gmail.com>
2023-07-31 17:21:14 +0530

    Button example addded/updated

---
+++ 3 AppCoda Tutorials/5 Buttons, Labels and Gradient/Button/ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonApp.swift
+//  Button
+//
+//  Created by EOO61 on 21/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
